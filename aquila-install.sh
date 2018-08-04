5dca4d5ddbb7654df9d76e2c2ea8f1a4c2e23bc4
TFinch <travisfinch01@gmail.com>
2018-08-04 14:44:23 -0400

    Update aquila-install.sh

--- aquila-install.sh
+++ aquila-install.sh
@@ -95,7 +95,7 @@ echo Now ready to setup AquilaX configuration file.
 
 RPCUSER=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 RPCPASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-VPSIP=$(curl -s4 icanhazip.com)
+EXTIP=`curl -s4 icanhazip.com`
 echo Please input your private key.
 read GENKEY
 
@@ -115,7 +115,7 @@ port=45454
 logtimestamps=1
 maxconnections=256
 masternode=1
-externalip=$VPSIP
+externalip=$EXTIP
 masternodeprivkey=$GENKEY
 addnode=139.99.195.25:45454
 addnode=139.99.198.86:45454
